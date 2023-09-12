dca1f4dcf3adf100eecc8644af00346385482167
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2023-09-12 23:47:49 +0600

    fix about version

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -73,7 +73,7 @@ class _AboutState extends State<About> {
                       ),
                       const SizedBox(height: 10),
                       const Text(
-                        'Version 1.3.19',
+                        'Version 1.3.20',
                         style: TextStyle(fontSize: 14),
                       ),
                     ],
